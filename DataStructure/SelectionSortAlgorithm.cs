2d3a0a90e0c5857a9ed63e16d1fd786408ffb8b9
sarangpatil2000@gmail.com <sarangpatil2000@gmail.com>
2019-09-16 22:17:11 +0800

    Added code for selection sort algorithm

--- DataStructure/SelectionSortAlgorithm.cs
+++ DataStructure/SelectionSortAlgorithm.cs
@@ -9,9 +9,43 @@ namespace DataStructure
             int[] inputarray = Common.AddInput();
             Console.WriteLine("Sorted ");
             Common.Print(SelectiomSortAlgo(inputarray));
+            Console.ReadLine();
         }
         public static int[] SelectiomSortAlgo(int[] inputArray)
         {
+            int value = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int minNumber = GetMinNumber(inputArray, i, out value);
+                inputArray = Swap(inputArray, i, value);
+            }
+            return inputArray;
+        }
+        public static int GetMinNumber(int[] input, int startIndex, out int position)
+        {
+            int minNumber = input[startIndex];
+            position = startIndex;
+            if (startIndex + 1 == input.Length)
+            {
+                return minNumber;
+            }
+            for (int i = startIndex; i < input.Length - 1; i++)
+            {
+                if (minNumber > input[i + 1])
+                {
+                    minNumber = input[i + 1];
+                    int value = i + 1;
+                    position = value;
+                    GetMinNumber(input, i + 1, out value);
+                }
+            }
+            return minNumber;
+        }
+        public static int[] Swap(int[] inputArray, int i, int minValueIndex)
+        {
+            int minVal = inputArray[minValueIndex];
+            inputArray[minValueIndex] = inputArray[i];
+            inputArray[i] = minVal;
             return inputArray;
         }
     }
