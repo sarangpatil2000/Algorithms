b2f6b4b81a3bf70e3f4ee056f7afd198222b67e8
sarangpatil2000@gmail.com <sarangpatil2000@gmail.com>
2020-11-01 00:44:53 +0800

    Binary Tree added

---
+++ DataStructure/BinaryTree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace DataStructure
+{
+    public class BinaryTree
+    {
+        public static void Main()
+        {
+            Node n1 = new Node(40);
+            n1.Add(20);
+            n1.Add(50);
+            n1.Add(10);
+            n1.Add(60);
+        }
+    }
+
+    internal class Node
+    {
+        public int Value { get; set; }
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+        internal Node(int value)
+        {
+            this.Value = value;
+        }
+
+        internal void Add(int value)
+        {
+            if (value < this.Value)
+            {
+                if (this.LeftNode == null)
+                {
+                    this.LeftNode = new Node(value);
+                }
+                else
+                {
+                    this.LeftNode.Add(value);
+                }
+            }
+            if (value > this.Value)
+            {
+                if(this.RightNode == null)
+                {
+                    this.RightNode = new Node(value);
+                }
+                else
+                {
+                    this.RightNode.Add(value);
+                }
+            }
+        }
+    }
+}
