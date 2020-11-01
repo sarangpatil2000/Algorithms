dc183283956f347321f38f06e0fb94de03fe341f
sarangpatil2000@gmail.com <sarangpatil2000@gmail.com>
2020-11-01 17:39:07 +0800

    Added Find, Pre-Order traversal

--- DataStructure/BinaryTree.cs
+++ DataStructure/BinaryTree.cs
@@ -3,13 +3,68 @@ namespace DataStructure
 {
     public class BinaryTree
     {
+        internal Node currentObject { get; set; }
         public static void Main()
         {
-            Node n1 = new Node(40);
-            n1.Add(20);
-            n1.Add(50);
-            n1.Add(10);
-            n1.Add(60);
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Add(40);
+            binaryTree.Add(20);
+            binaryTree.Add(50);
+            binaryTree.Add(25);
+            binaryTree.Add(60);
+            binaryTree.Add(100);
+            binaryTree.Add(1);
+
+            Console.WriteLine(" finding 1 : " +  binaryTree.Find(1).ToString());
+            Console.WriteLine(" finding 100 : " + binaryTree.Find(100).ToString());
+            Console.WriteLine(" finding 1000 : " + binaryTree.Find(1000).ToString());
+            Console.WriteLine(" finding 50 : " + binaryTree.Find(50).ToString());
+
+            binaryTree.PreDepthTraversal(binaryTree.currentObject);
+            Console.ReadLine();
+        }
+        private void Add(int value)
+        {
+            if(currentObject == null)
+            {
+                currentObject = new Node(value);
+                return;
+            }
+            currentObject.Add(value);
+        }
+        public bool Find(int value)
+        {
+            Node presentNode =this.currentObject;
+            while (presentNode != null)
+            {
+                if(value < presentNode.Value)
+                {
+                    if (presentNode.LeftNode != null) { presentNode = presentNode.LeftNode; } else return false;
+                    
+                }
+                if (value > presentNode.Value)
+                {
+                    if (presentNode.RightNode != null) { presentNode = presentNode.RightNode; } else return false;
+                    
+                }
+                if(value == presentNode.Value)
+                {
+                    presentNode = null;
+                    return true;
+                }
+            }
+            return false;
+        }
+        internal void PreDepthTraversal(Node sendNode)
+        {
+            Node presentNode = sendNode;
+            Console.Write(presentNode.Value + ",");
+            if (presentNode.LeftNode != null)
+            {
+                PreDepthTraversal(presentNode.LeftNode);
+            }
+            if(presentNode.RightNode != null)
+            { PreDepthTraversal(presentNode.RightNode); }
         }
     }
 
